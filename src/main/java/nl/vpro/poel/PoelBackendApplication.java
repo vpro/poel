8b64bea97798c77ca94b3d69133737f478a629c0
Nils Breunese <nils@breun.nl>
2016-05-04 12:16:13 +0200

    Spring Boot skeleton.

---
+++ src/main/java/nl/vpro/poel/PoelBackendApplication.java
@@ -0,0 +1,12 @@
+package nl.vpro.poel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoelBackendApplication.class, args);
+	}
+}
