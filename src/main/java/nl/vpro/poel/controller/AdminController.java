300d9f71e71977ffb79447f5d9cb464463c4bed2
Nils Breunese <nils@breun.nl>
2016-05-10 23:47:38 +0200

    /admin toont nu een lijst van alle gebruikers.

--- src/main/java/nl/vpro/poel/controller/AdminController.java
+++ src/main/java/nl/vpro/poel/controller/AdminController.java
@@ -1,13 +1,28 @@
 package nl.vpro.poel.controller;
 
+import nl.vpro.poel.domain.User;
+import nl.vpro.poel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 class AdminController {
 
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/admin")
-    String admin() {
+    String admin(Model model) {
+        List<User> allUsers = userService.getAllUsers();
+        model.addAttribute("users", allUsers);
         return "admin";
     }
 }
--- src/main/resources/templates/admin.ftl
+++ src/main/resources/templates/admin.ftl
@@ -2,5 +2,16 @@
 <html lang="nl">
 <body>
 Alleen admins zoals ${user.displayName} (${user.username}/${user.role}) kunnen dit zien!
+
+<#list users>
+    <h2>Alle deelnemers</h2>
+    <ul>
+        <#items as u>
+            <li>${u.displayName} (${u.username})</li>
+        </#items>
+    </ul>
+<#else>
+    Er zijn geen deelnemers. :o(
+</#list>
 </body>
 </html>
