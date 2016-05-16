1a4433bceeca68fd70e60ece71002601070550c8
Nils Breunese <nils@breun.nl>
2016-05-16 18:41:16 +0200

    Use the List<Prediction>, Luke!

---
+++ src/main/java/nl/vpro/poel/dto/Prediction.java
@@ -0,0 +1,43 @@
+package nl.vpro.poel.dto;
+
+public class Prediction {
+
+    private Long matchId;
+
+    private int homeTeamGoals;
+
+    private int awayTeamGoals;
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getHomeTeamGoals() {
+        return homeTeamGoals;
+    }
+
+    public void setHomeTeamGoals(int homeTeamGoals) {
+        this.homeTeamGoals = homeTeamGoals;
+    }
+
+    public int getAwayTeamGoals() {
+        return awayTeamGoals;
+    }
+
+    public void setAwayTeamGoals(int awayTeamGoals) {
+        this.awayTeamGoals = awayTeamGoals;
+    }
+
+    @Override
+    public String toString() {
+        return "Prediction{" +
+                "matchId=" + matchId +
+                ", homeTeamGoals=" + homeTeamGoals +
+                ", awayTeamGoals=" + awayTeamGoals +
+                '}';
+    }
+}
--- src/main/java/nl/vpro/poel/dto/PredictionForm.java
+++ src/main/java/nl/vpro/poel/dto/PredictionForm.java
@@ -1,52 +1,17 @@
 package nl.vpro.poel.dto;
 
+import java.util.List;
+
 // TODO: Validator
 public class PredictionForm {
 
-    private Long matchId;
+    private List<Prediction> predictions;
 
-    private int homeTeamGoals;
-
-    private int awayTeamGoals;
-
-    private PredictionForm() {}
-
-    public PredictionForm(Long matchId, int homeTeamGoals, int awayTeamGoals) {
-        this.matchId = matchId;
-        this.homeTeamGoals = homeTeamGoals;
-        this.awayTeamGoals = awayTeamGoals;
+    public List<Prediction> getPredictions() {
+        return predictions;
     }
 
-    public Long getMatchId() {
-        return matchId;
-    }
-
-    public void setMatchId(Long matchId) {
-        this.matchId = matchId;
-    }
-
-    public int getHomeTeamGoals() {
-        return homeTeamGoals;
-    }
-
-    public void setHomeTeamGoals(int homeTeamGoals) {
-        this.homeTeamGoals = homeTeamGoals;
-    }
-
-    public int getAwayTeamGoals() {
-        return awayTeamGoals;
-    }
-
-    public void setAwayTeamGoals(int awayTeamGoals) {
-        this.awayTeamGoals = awayTeamGoals;
-    }
-
-    @Override
-    public String toString() {
-        return "PredictionForm{" +
-                "matchId=" + matchId +
-                ", homeTeamGoals=" + homeTeamGoals +
-                ", awayTeamGoals=" + awayTeamGoals +
-                '}';
+    public void setPredictions(List<Prediction> predictions) {
+        this.predictions = predictions;
     }
 }
