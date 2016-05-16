6f94ed34adb04506beb0c6fdfe2c5539eb28cb34
Nils Breunese <nils@breun.nl>
2016-05-16 17:46:26 +0200

    PredictionForm d'rbij.

---
+++ src/main/java/nl/vpro/poel/dto/PredictionForm.java
@@ -0,0 +1,50 @@
+package nl.vpro.poel.dto;
+
+// TODO: Validator
+public class PredictionForm {
+
+    private Long matchId;
+
+    private int homeTeamGoals;
+
+    private int awayTeamGoals;
+
+    public PredictionForm(Long matchId, int homeTeamGoals, int awayTeamGoals) {
+        this.matchId = matchId;
+        this.homeTeamGoals = homeTeamGoals;
+        this.awayTeamGoals = awayTeamGoals;
+    }
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public int getHomeTeamGoals() {
+        return homeTeamGoals;
+    }
+
+    public int getAwayTeamGoals() {
+        return awayTeamGoals;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PredictionForm that = (PredictionForm) o;
+
+        if (homeTeamGoals != that.homeTeamGoals) return false;
+        if (awayTeamGoals != that.awayTeamGoals) return false;
+        return matchId.equals(that.matchId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = matchId.hashCode();
+        result = 31 * result + homeTeamGoals;
+        result = 31 * result + awayTeamGoals;
+        return result;
+    }
+}
