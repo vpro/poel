3d3c3470cfc4bbb230aa766fcab0c917d9435025
Nils Breunese <nils@breun.nl>
2016-05-21 12:48:28 +0200

    Useless import, Luke!

--- src/main/java/nl/vpro/poel/PoelApplication.java
+++ src/main/java/nl/vpro/poel/PoelApplication.java
@@ -1,13 +1,10 @@
 package nl.vpro.poel;
 
-import nl.vpro.poel.configuration.CasSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = VelocityAutoConfiguration.class) // Spring Security CAS brings in Velocity, but we want to use Freemarker as our template engine
-@Import(CasSecurityConfiguration.class)
 public class PoelApplication {
 
 	public static void main(String[] args) {
