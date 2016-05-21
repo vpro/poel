d488efd8664646f14ee5fd5ef0fe5d0a15ecd0dc
Nils Breunese <nils@breun.nl>
2016-05-21 12:11:15 +0200

    Gebruiken we niet.

--- src/main/java/nl/vpro/poel/PoelApplication.java
+++ src/main/java/nl/vpro/poel/PoelApplication.java
@@ -4,12 +4,9 @@ import nl.vpro.poel.configuration.CasSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication(exclude = VelocityAutoConfiguration.class) // Spring Security CAS brings in Velocity, but we want to use Freemarker as our template engine
-@EntityScan(basePackageClasses = { PoelApplication.class, Jsr310JpaConverters.class }) // Remove this annotation after upgrading to Hibernate 5.x, as that natively supports persisting java.time.* classes
 @Import(CasSecurityConfiguration.class)
 public class PoelApplication {
 
