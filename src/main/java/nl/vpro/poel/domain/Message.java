aa3e49a83fb52ae4ca39adee333e8891ac7370a6
frenkie <frank@frankbosma.nl>
2016-05-23 01:39:06 +0200

    Saving messages works!

--- src/main/java/nl/vpro/poel/domain/Message.java
+++ src/main/java/nl/vpro/poel/domain/Message.java
@@ -28,9 +28,13 @@ public class Message {
         return id;
     }
 
+    public void setId( Long id ) { this.id = id; }
+
     public String getKey() {
         return key;
     }
+
+    public void setKey ( String key ) { this.key = key; }
 
     public String getText() {
         return text;
