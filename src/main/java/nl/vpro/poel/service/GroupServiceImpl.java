562eea1bb92dd5920f45b85b1c7cc4e92cf63656
Nils Breunese <nils@breun.nl>
2016-05-24 19:23:14 +0200

    Remove the unused logger (for the wrong class), Luke!

--- src/main/java/nl/vpro/poel/service/GroupServiceImpl.java
+++ src/main/java/nl/vpro/poel/service/GroupServiceImpl.java
@@ -1,23 +1,16 @@
 package nl.vpro.poel.service;
 
-
 import nl.vpro.poel.domain.Group;
 import nl.vpro.poel.dto.GroupForm;
 import nl.vpro.poel.repository.GroupRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class GroupServiceImpl implements GroupService {
-
-    private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
 
     private final GroupRepository groupRepository;
 
