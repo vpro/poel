c6028f34d5e17be8e8eb7d1daaa7ac74f63c9956
Nils Breunese <nils@breun.nl>
2016-05-25 17:31:02 +0200

    Correct winner/wrong match result is worth only 1 point, Luke!

--- src/test/java/nl/vpro/poel/service/ScoreServiceImplTest.java
+++ src/test/java/nl/vpro/poel/service/ScoreServiceImplTest.java
@@ -3,62 +3,73 @@ package nl.vpro.poel.service;
 import nl.vpro.poel.domain.Match;
 import nl.vpro.poel.domain.MatchResult;
 import nl.vpro.poel.domain.Prediction;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreServiceImplTest {
 
-    private ScoreService scoreService = new ScoreServiceImpl(null, null, 2, 1, 2);
+    private ScoreService scoreService;
+
+    @Before
+    public void setup() {
+        UserService userService = null;
+        PredictionService predictionService = null;
+        int pointsForCorrectWinner = 1;
+        int pointsForCorrectMatchResult = 2;
+        int scoreMultiplierFactor = 2;
+        scoreService = new ScoreServiceImpl(userService, predictionService, pointsForCorrectWinner, pointsForCorrectMatchResult, scoreMultiplierFactor);
+    }
 
     @Test
     public void scoreForCorrectWinnerAndResult() {
-        checkScore(new MatchResult(0, 0), new MatchResult(0, 0), 3);
+        checkScore("0-0", "0-0", 3);
     }
 
     @Test
     public void scoreForCorrectWinnerAndResultWithMultiplier() {
-        checkScoreWithMultiplier(new MatchResult(1, 3), new MatchResult(1, 3), 6);
+        checkScoreWithMultiplier("1-3", "1-3", 6);
     }
 
     @Test
     public void scoreForCorrectWinnerWrongResult() {
-        checkScore(new MatchResult(1, 0), new MatchResult(2, 0), 2);
+        checkScore("1-0", "2-0", 1);
     }
 
     @Test
     public void scoreForCorrectWinnerWrongResultWithMultiplier() {
-        checkScoreWithMultiplier(new MatchResult(5, 0), new MatchResult(1, 0), 4);
+        checkScoreWithMultiplier("5-0", "1-0", 2);
     }
 
     @Test
     public void scoreForWrongWinner() {
-        checkScore(new MatchResult(1, 0), new MatchResult(0, 1), 0);
+        checkScore("1-0", "0-1", 0);
     }
 
     @Test
     public void scoreForWrongWinnerWithMultiplier() {
-        checkScoreWithMultiplier(new MatchResult(5, 4), new MatchResult(3, 3), 0);
+        checkScoreWithMultiplier("5-4", "3-3", 0);
     }
 
     @Test
     public void scoreForNoPredictedResult() {
-        checkScore(null, new MatchResult(1, 0), 0);
+        checkScore(null, "1-0", 0);
     }
 
     @Test
     public void scoreForNoPredictedResultWithMultiplier() {
-        checkScore(null, new MatchResult(1, 0), 0);
+        checkScore(null, "1-0", 0);
     }
 
     @Test
     public void scoreForNoActualResult() {
-        checkScore(new MatchResult(1, 0), null, 0);
+        checkScore("1-0", null, 0);
     }
 
     @Test
     public void scoreForNoActualResultWithMultiplier() {
-        checkScoreWithMultiplier(new MatchResult(1, 0), null, 0);
+        checkScoreWithMultiplier("1-0", null, 0);
     }
 
     @Test
@@ -71,17 +82,29 @@ public class ScoreServiceImplTest {
         checkScoreWithMultiplier(null, null, 0);
     }
 
-    private void checkScoreWithMultiplier(MatchResult predictedResult, MatchResult actualResult, int expectedScore) {
+    private void checkScoreWithMultiplier(String predictedResult, String actualResult, int expectedScore) {
         checkScore(predictedResult, actualResult, true, expectedScore);
     }
 
-    private void checkScore(MatchResult predictedResult, MatchResult actualResult, int expectedScore) {
+    private void checkScore(String predictedResult, String actualResult, int expectedScore) {
         checkScore(predictedResult, actualResult, false, expectedScore);
     }
 
-    private void checkScore(MatchResult predictedResult, MatchResult actualResult, boolean multiplier, int expectedScore) {
+    private void checkScore(String predicted, String actual, boolean multiplier, int expectedScore) {
+        MatchResult predictedResult = toMatchResult(predicted);
+        MatchResult actualResult = toMatchResult(actual);
         Prediction prediction = new Prediction(null, new Match("Home", "Away", null, actualResult), predictedResult, multiplier);
         int actualScore = scoreService.getScore(prediction);
         assertThat(actualScore).isEqualTo(expectedScore);
+    }
+
+    private MatchResult toMatchResult(String matchResult) {
+        if (matchResult == null) {
+            return null;
+        }
+        String[] goals = matchResult.split("-");
+        int homeTeamGoals = Integer.valueOf(goals[0]);
+        int awayTeamGoals = Integer.valueOf(goals[1]);
+        return new MatchResult(homeTeamGoals, awayTeamGoals);
     }
 }
