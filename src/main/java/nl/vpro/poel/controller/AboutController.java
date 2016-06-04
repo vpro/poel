8041931d93558537539033d1ba0d376a7f897dd6
Nils Breunese <nils@breun.nl>
2016-06-04 10:13:34 +0200

    Don't use the Principal, Luke!

--- src/main/java/nl/vpro/poel/controller/AboutController.java
+++ src/main/java/nl/vpro/poel/controller/AboutController.java
@@ -2,14 +2,12 @@ package nl.vpro.poel.controller;
 
 import nl.vpro.poel.domain.Bulletin;
 import nl.vpro.poel.service.BulletinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -18,13 +16,13 @@ public class AboutController {
 
     private final BulletinService bulletinService;
 
+    @Autowired
     public AboutController(BulletinService bulletinService) {
         this.bulletinService = bulletinService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    String showAbout(Principal principal, Model model) {
-
+    String showAbout(Model model) {
         List<Bulletin> bulletins = bulletinService.findAll();
         model.addAttribute("bulletins", bulletins);
         return "about";
