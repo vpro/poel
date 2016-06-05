80a382bbff03aaf0005bfa2301336c923b7f2692
Nils Breunese <nils@breun.nl>
2016-06-05 11:49:52 +0200

    Allow posting collections with more than 256 items, Luke!

--- src/main/java/nl/vpro/poel/controller/PoelControllerAdvice.java
+++ src/main/java/nl/vpro/poel/controller/PoelControllerAdvice.java
@@ -5,14 +5,16 @@ import nl.vpro.poel.domain.CurrentUser;
 import nl.vpro.poel.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 /**
- * This class adds model attributes that are common to all controllers.
+ * This class sets configuration and model attributes for all controllers.
  */
 @ControllerAdvice
 public class PoelControllerAdvice {
@@ -25,6 +27,11 @@ public class PoelControllerAdvice {
     public PoelControllerAdvice(MessageService messageService, @Value("${poel.title}") String title) {
         this.messageService = messageService;
         this.title = title;
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAutoGrowCollectionLimit(1024); // By default 256 is the max and we might get a POST with more users than that
     }
 
     @ModelAttribute("user")
