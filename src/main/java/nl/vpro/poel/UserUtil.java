c24a92de79a07da74946d5dfcf5cb0e55e7a9918
Nils Breunese <nils@breun.nl>
2016-07-31 12:30:31 +0200

    Use Lombok's UtilityClass.

--- src/main/java/nl/vpro/poel/UserUtil.java
+++ src/main/java/nl/vpro/poel/UserUtil.java
@@ -1,5 +1,6 @@
 package nl.vpro.poel;
 
+import lombok.experimental.UtilityClass;
 import nl.vpro.poel.domain.CurrentUser;
 import org.springframework.security.cas.authentication.CasAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,9 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.security.Principal;
 import java.util.Optional;
 
+@UtilityClass
 public class UserUtil {
 
-    public static Optional<CurrentUser> getCurrentUser(Principal principal) {
+    public Optional<CurrentUser> getCurrentUser(Principal principal) {
         if (principal instanceof CasAuthenticationToken) {
             UserDetails userDetails = ((CasAuthenticationToken)principal).getUserDetails();
             if (userDetails instanceof CurrentUser) {
